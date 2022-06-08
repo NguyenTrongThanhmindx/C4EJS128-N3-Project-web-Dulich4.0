a641d4d6b2a23db88ef4f4fc5ffe504788e089a5
Thanh Nguyen <thanhnguyendhnt95@gmail.com>
2022-06-08 18:39:49 +0700

    complete

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let searchBtn = document.querySelector('#search-btn');
 let searchBar = document.querySelector('.search-bar-container');
 let formBtn = document.querySelector('#login-btn');
+let logoutBtn = document.querySelector('#logout-btn');
 let loginForm = document.querySelector('.login-form-container');
 let formClose = document.querySelector('#form-close');
 
@@ -90,4 +91,76 @@ var swiper = new Swiper(".brand-slider", {
       },
 });
 
+function User(username, pass) {
+  this.username = username;
+  this.pass = pass;
+}
 
+function activeformLogin() {
+  loginForm.classList.add('active');
+}
+
+function setCurrentUser(u) {
+  window.localStorage.setItem('CurrentUser', JSON.stringify(u));
+}
+
+function equalUser(u1, u2) {
+	return (u1.username == u2.username && u1.pass == u2.pass);
+}
+
+function getListUser() {
+  var data = JSON.parse(window.localStorage.getItem('ListUser')) || []
+  var l = [];
+  for (var d of data) {
+      l.push(d);
+  }
+  return l;
+}
+
+function logIn(form) {
+  // Lấy dữ liệu từ form
+  var name = form.username.value;
+  var pass = form.password.value;
+  var newUser = new User(name, pass);
+
+  // Lấy dữ liệu từ danh sách người dùng localstorage
+  var listUser = getListUser();
+
+  // Kiểm tra xem dữ liệu form có khớp với người dùng nào trong danh sách ko
+  for (var u of listUser) {
+    if (equalUser(newUser, u)) {
+        if(u.off) {
+            alert('Tài khoản này đang bị khoá. Không thể đăng nhập.');
+            return false;
+        }
+
+        setCurrentUser(u);
+
+        // Reload lại trang -> sau khi reload sẽ cập nhật luôn giỏ hàng khi hàm setupEventTaiKhoan chạy
+        location.reload();
+        return false;
+    }
+  }
+
+  alert('Nhập sai tên hoặc mật khẩu !!!');
+  form.username.focus();
+  return false;
+}
+
+function logOut() {
+  window.localStorage.removeItem('CurrentUser');
+  location.reload();
+}
+
+function getCurrentUser() {
+  return JSON.parse(window.localStorage.getItem('CurrentUser')); // Lấy dữ liệu từ localstorage
+}
+
+let checkUser = document.querySelector('#check-user-login');
+
+if(!getCurrentUser()) {
+  checkUser.innerHTML = '<i class="fas fa-search" id="search-btn"></i><i class="fas fa-user" onclick="activeformLogin()"></i>';
+}
+else {
+  checkUser.innerHTML = '<i class="fas fa-search" id="search-btn"></i><i class="fas fa-power-off" onclick="logOut()"></i>';
+}
